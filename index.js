2efbe843c3936ce772ef4b3d217e47cb62601369
Andrew McDowell <madoliole@gmail.com>
2016-06-19 13:40:22 +1000

    Adding eslintrc, formatting

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, Tray, Menu, BrowserWindow } = require('electron');
+const { app, Tray, Menu } = require('electron');
 const path = require('path');
 const childProcess = require('child_process');
 
@@ -10,56 +10,56 @@ const chromeIconPath = path.join(__dirname, 'icons/chrome.png');
 const firefoxIconPath = path.join(__dirname, 'icons/firefox.png');
 
 let appIcon = null;
-let win = null;
 
-app.on('ready', function(){
-  win = new BrowserWindow({show: false});
-  appIcon = new Tray(iconPath);
-  var contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'atom',
-      icon: atomIconPath,
-      accelerator: 'Command+A',
-      click: function() {
-          childProcess.exec('atom').unref();
-      }
-    },
-    {
-      label: 'VSCode',
-      icon: vscodeIconPath,
-      accelerator: 'Command+V',
-      click: function() {
-          childProcess.exec('VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;').unref();
-      }
-    },
-    {
-      label: 'iTerm',
-      icon: iTermIconPath,
-      accelerator: 'Command+I',
-      click: function() {
-          childProcess.exec('open -a iterm').unref();
-      }
-    },
-    {
-      label: 'Chrome',
-      icon: chromeIconPath,
-      accelerator: 'Command+C',
-      click: function() {
-          childProcess.exec('open -a "Google Chrome"').unref();
-      }
-    },
-    {
-      label: 'Firefox',
-      icon: firefoxIconPath,
-      accelerator: 'Command+F',
-      click: function() {
-          childProcess.exec('open -a firefox').unref();
-      }
-    },
+app.on('ready', () => {
+    appIcon = new Tray(iconPath);
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: 'atom',
+            icon: atomIconPath,
+            accelerator: 'Command+A',
+            click() {
+                childProcess.exec('atom').unref();
+            }
+        },
+        {
+            label: 'VSCode',
+            icon: vscodeIconPath,
+            accelerator: 'Command+V',
+            click() {
+                childProcess
+                    .exec('VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;')
+                    .unref();
+            }
+        },
+        {
+            label: 'iTerm',
+            icon: iTermIconPath,
+            accelerator: 'Command+I',
+            click() {
+                childProcess.exec('open -a iterm').unref();
+            }
+        },
+        {
+            label: 'Chrome',
+            icon: chromeIconPath,
+            accelerator: 'Command+C',
+            click() {
+                childProcess.exec('open -a "Google Chrome"').unref();
+            }
+        },
+        {
+            label: 'Firefox',
+            icon: firefoxIconPath,
+            accelerator: 'Command+F',
+            click() {
+                childProcess.exec('open -a firefox').unref();
+            }
+        },
     { label: 'Quit',
       accelerator: 'Command+Q',
-      selector: 'terminate:',
+      selector: 'terminate:'
     }
-  ]);
-  appIcon.setContextMenu(contextMenu);
+    ]);
+    appIcon.setContextMenu(contextMenu);
 });
